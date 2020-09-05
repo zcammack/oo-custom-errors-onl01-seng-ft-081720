8a52cb7263805a184d83494c3e8fc8eeded6af04
Zackary Cammack <zack.cammack@gmail.com>
2020-09-05 15:19:28 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,7 +10,7 @@ class Person
     if person.class != Person
       raise PartnerError
     else
-    person.partner = self
+      person.partner = self
     end
   end
   class PartnerError < StandardError
