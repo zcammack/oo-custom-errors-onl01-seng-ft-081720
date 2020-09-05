672446bb7da3f392af45104bdcceda88db203dcd
Zackary Cammack <zack.cammack@gmail.com>
2020-09-05 15:17:23 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,13 +9,11 @@ class Person
     self.partner = person
     person.partner = self
   end
-
+  class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
-
-class PartnerError < StandardError
-end
