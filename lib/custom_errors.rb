f7083b12809b3bbfbbbcc7f6fa384629af661c69
Zackary Cammack <zack.cammack@gmail.com>
2020-09-05 15:19:52 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -4,15 +4,14 @@ class Person
   def initialize(name)
     @name = name
   end
-
   def get_married(person)
-    self.partner = person\
-    if person.class != Person
-      raise PartnerError
-    else
-      person.partner = self
+      self.partner = person
+      if person.class != Person
+        raise PartnerError
+      else
+        person.partner = self
+      end
     end
-  end
   class PartnerError < StandardError
   end
 end
