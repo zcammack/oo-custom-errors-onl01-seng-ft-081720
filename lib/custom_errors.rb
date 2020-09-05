b582ea9d513d530aa55e509d511c79edae124cc0
Zackary Cammack <zack.cammack@gmail.com>
2020-09-05 15:22:28 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,11 @@ class Person
   def get_married(person)
       self.partner = person
       if person.class != Person
-        raise PartnerError
+        begin
+          raise PartnerError
+        rescue PartnerError => error
+          puts error.message
+        end
       else
         person.partner = self
       end
